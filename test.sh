84b99122d9367d4c8d03f9678b8a95ef0877b4a2
lcg <lcgcf2013@163.com>
2022-07-26 10:17:43 +0800

    add new training model for tiny_640

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-python tools/demo.py image -f exps/example/yolox_voc/yolox_voc_nano.py \
--c YOLOX_outputs/yolox_voc_nano/best_ckpt.pth \
---path testfiles/img1.jpg --conf 0.3 --nms 0.65 
---tsize 640 --save_result --device cpu
+python tools/demo.py image -f exps/example/yolox_voc/yolox_voc_tiny_640.py \
+-c YOLOX_outputs/yolox_voc_tiny_640/best_ckpt.pth \
+--path /home/lcg/Downloads/save_image --conf 0.3 --nms 0.65 \
+--tsize 640 --save_result  --device 0
--- train_tiny_640.sh
+++ train_tiny_640.sh
@@ -1,2 +1,2 @@
 export YOLOX_DATADIR="/home/lcg/Downloads/TrainingData"
-python tools/train.py -f exps/example/yolox_voc/yolox_voc_tiny_640.py -d 0 -b 72 --fp16 -o -c weights/yolox_tiny.pth --cache
+python tools/train.py -f exps/example/yolox_voc/yolox_voc_tiny_640.py -d 0 -b 68 --fp16 -o -c weights/yolox_tiny.pth --cache
